7caec2679cda62ca26a07b44f3ddbcaa3bb6165f
Grytale <152791180+Grytale@users.noreply.github.com>
2024-04-01 16:39:43 +0300

    Add files via upload

---
+++ Tema-Vremea/index.js
@@ -0,0 +1,20 @@
+//Luam valoarea cheii city din local storage
+let currentCityFromLS = localStorage.getItem("city");
+
+// pasul urmator este sa actualizam orasul afisat pe ecran daca avem cheia "city" in  localStorage
+const currentCityTag = document.querySelector(".current-city");
+if (currentCityFromLS) {
+  currentCityTag.innerHTML = currentCityFromLS;
+}
+
+// daca nu avem key city in localStorage atunci setam ca valoare default Bucuresti - atat pentru localStorage cat si pentru variabila currentCityFrom LS
+if (!currentCityFromLS) {
+  localStorage.setItem("city", "Bucuresti");
+  currentCityFromLS = "Bucuresti";
+}
+
+// Afisam vremea curenta folosind o functie din alt fisier
+displayCurrentWeather(currentCityFromLS);
+
+// Afisam vremea pe urmatoarele 5 zile
+displayWeatherForecast(currentCityFromLS);
